abda0a04a1a94c4987df96a49dc28abc9036f81b
unknown <p.sanooj@gmail.com>
2020-03-29 23:12:00 +0530

    linear regression - notes updated

--- preprocessing/nv/NVUtil.py
+++ preprocessing/nv/NVUtil.py
@@ -2,6 +2,14 @@ def nv_treatment(df,nv_threshold):
     print()
     print('Before NV treatment the stats are as below')
     print(df.isna().sum())
+    null_value_exclude_filter = (df.isna().sum()/df.shape[0])*100
+    selected_columns = null_value_exclude_filter[null_value_exclude_filter < nv_threshold]
+    excluded_columns = null_value_exclude_filter[null_value_exclude_filter > nv_threshold]
+    selected_columns = selected_columns.index
+    excluded_columns = excluded_columns.index
+    
+    df.drop(excluded_columns,axis=1,inplace=True)
+    
     #numerical_columns = df.mean().index
     # changed from mean() to describe() because , boolean values are considered as integers while using mean()
     numerical_columns = df.describe().columns.values
@@ -10,13 +18,6 @@ def nv_treatment(df,nv_threshold):
     for col in df.columns:
         if col not in numerical_columns:
             categorical_columns.append(col)
-    null_value_exclude_filter = (df.isna().sum()/df.shape[0])*100
-    selected_columns = null_value_exclude_filter[null_value_exclude_filter < nv_threshold]
-    excluded_columns = null_value_exclude_filter[null_value_exclude_filter > nv_threshold]
-    selected_columns = selected_columns.index
-    excluded_columns = excluded_columns.index
-    
-    df.drop(excluded_columns,axis=1,inplace=True)
     
     for col in df.columns:
         if col in numerical_columns:
